e9dc4a4d878fbacc0b589ebd4d3d35f5718b875c
Brian Stovall <hoppenmaus@gmail.com>
2016-02-01 17:21:25 -0500

    navbar and carousel built

--- news.js
+++ news.js
@@ -8,7 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	newsRequest.onload = function() {
 		if (newsRequest.status >=200 && newsRequest.status <= 400) {
 			console.log('success rec\'d: ' + newsRequest.responseText);
+			populate(newsRequest.responseText);
 	  }
 		else console.log('fail... rec\'d: '  + newsRequest.responseText);
+	}
+
+	//populate with the top 10 news stories based on the desired field
+	function populate() {
 	}
 });
