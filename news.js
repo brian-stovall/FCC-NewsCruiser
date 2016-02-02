fcd688c5c5a9f66d0b36dbf3cebef2ffc11eb60f
Brian Stovall <hoppenmaus@gmail.com>
2016-02-01 21:25:36 -0500

    added glyphicon buttons to carousel. added sorting feature. linked entries to articles and authors.

--- news.js
+++ news.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	var grab = document.getElementById.bind(document);
-	var slides = document.getElementsByClassName('item');
 	var newsCarousel = grab('newsCarousel');
+	var sortPopularity = grab('sortByPopularity');
+	var sortTime = grab('sortByTime');
+
+	var slides = document.getElementsByClassName('item');
 
 	//set up and obtain the api data
 	var newsRequest = new XMLHttpRequest();
 	var apiURL = 'http://www.freecodecamp.com/news/hot';
+
+	//store the JSON retreived from the API
+	var data;
 
 	newsRequest.open('GET', apiURL);
 	newsRequest.send();
@@ -14,17 +20,42 @@ document.addEventListener('DOMContentLoaded', () => {
 	newsRequest.onload = function() {
 		if (newsRequest.status >=200 && newsRequest.status <= 400) {
 			console.log('success rec\'d: ' + newsRequest.responseText);
-			populate(JSON.parse(newsRequest.responseText));
+			data = (JSON.parse(newsRequest.responseText));
+			populate();
 	  }
 		else console.log('fail... rec\'d: '  + newsRequest.responseText);
 	}
 
+	//sort the data by popularity or time posted
+	function sortData(sortfield) {
+		if (sortfield === 'popularity')
+			data = data.sort( (cur, prev) => {return prev.rank - cur.rank;});
+		else
+			data = data.sort( (cur, prev) => {return prev.timePosted - cur.timePosted;});
+	}
+
+	sortTime.onclick = () => {
+		sortData('time');
+		populate();
+	}
+
+	//
+
 	//populate with the top 10 news stories based on the desired field
-	function populate(data) {
+	function populate() {
+		sortData('popularity');
+
 		for (var i = 0; i < 10; i++)
 			populateSlide(slides[i], data[i]);
 
-		$('#newsCarousel').carousel();
+	//make the carousel controls visible
+	var controls = document.getElementsByClassName('carousel-control');
+	for (i = 0; i < controls.length; i++)
+		controls[i].style.visibility='visible';
+
+  //start the carousel
+	$('#newsCarousel').carousel({interval:false});
+	
 	}
 
 	//helper function that populates a single slide element
@@ -36,8 +67,14 @@ document.addEventListener('DOMContentLoaded', () => {
 		//now load the text fields
 		slide.getElementsByTagName('h1')[0].textContent = entry.headline;
 		slide.getElementsByClassName('auth')[0].textContent = 'by ' + entry.author.username;
-		slide.getElementsByClassName('rank')[0].textContent = entry.rank;
-		slide.getElementsByClassName('time')[0].textContent = entry.timePosted;
+		slide.getElementsByClassName('rank')[0].textContent = entry.rank + ' upvotes';
+		slide.getElementsByClassName('time')[0].textContent = 
+			new Date(entry.timePosted).toDateString();
+
+		//make the links
+		slide.getElementsByClassName('articleLink')[0].href = entry.link;
+		slide.getElementsByClassName('profileLink')[0].href = 'http://www.freecodecamp/' 
+			+ entry.author.username;
 	}
 
 });
